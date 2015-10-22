0062512b44f78daca2b07f0710cf05f0473c790e
Thomas Berger <thomas@epicentral.de>
2015-10-23 00:57:19 +0200

    Changed configuration of webcam endpoint to work with MacBook iSight camera

--- src/main/java/de/thberger/camelcam/WebCamRouter.java
+++ src/main/java/de/thberger/camelcam/WebCamRouter.java
@@ -5,10 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * Camel application with webcam route
- *
+ * <p>
  * Taken from:
  * http://gautric.github.io/blog/2015/10/22/rhiot-0.1.2-camel-webcam-macos-x.html
- *
+ * <p>
  * Configured as Spring Boot application.
  *
  * @author Thomas Berger
@@ -16,9 +16,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WebCamRouter extends FatJarRouter {
 
+    /**
+     * Default resolution of Webcam endpoint is 320x240, which throws
+     * error if used on MacBook with iSight camera.
+     */
+    private static final int WIDTH = 1280;
+    private static final int HEIGHT = 720;
+
     @Override
     public void configure() throws Exception {
-        from("webcam:cam?consumer.delay=1000")
+        from("webcam:cam?width=" + WIDTH + "&height=" + HEIGHT + "&consumer.delay=1000")
                 .to("log://io.rhiot?showAll=true")
                 .to("file://webapp");
     }
